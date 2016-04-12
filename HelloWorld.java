c52d493391711fd203f69a872b44a536ac70d36d
Erik <Deciver_89@hotmail.com>
2016-04-12 17:26:40 +0200

    Added files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        // Basic function
+        System.out.println("Hello World!!");
+    }
+    public void testMethod(){
+    // for testing in Github
+    }
+    
+}
