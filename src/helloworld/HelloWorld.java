3753467f8a3a2e55924602a965e3348780fdf659
Erik <Deciver_89@hotmail.com>
2016-04-12 19:13:44 +0200

    hmm

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        // Basic function
+        System.out.println("Hello World!!");
+    }
+    public void testMethod(){
+    // for testing in Github
+        System.out.println("Hej");
+    }
+    
+}
